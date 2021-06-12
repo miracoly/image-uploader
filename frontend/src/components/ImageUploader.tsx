48eb29fe1e6f34602f815e232e4b647c8efe50a0
miracoly <68049792+miracoly@users.noreply.github.com>
2021-06-12 10:19:11 +0200

    add to frontend

--- frontend/src/components/ImageUploader.tsx
+++ frontend/src/components/ImageUploader.tsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, {BaseSyntheticEvent, useState} from "react";
 import Icon from "../image.svg";
 import "./ImageUploader.css";
 import {Box, Button, Typography} from "@material-ui/core";
 
 function ImageUploader() {
 
+    const [imageUrl, setImageUrl] = useState("");
+
+    function handleFileSelection(event: BaseSyntheticEvent) {
+        event.preventDefault()
+        try {
+            const imageFile: FileList = event.target.files[0];
+            setImageUrl(URL.createObjectURL(imageFile));
+        } catch (error) {
+            console.log("Error while uploading file");
+        }
+    }
+
     return (
         <Box className="container">
             <Typography variant="h4" component="h1" gutterBottom>
                 Upload your image
             </Typography>
-            <Typography variant="body1" gutterBottom>File should be jpeg, png...</Typography>
-                <Box className="drag-n-drop">
-                    <img src={Icon} alt="mountain" className="image"/>
-                    <Typography variant="body2">Drag & Drop your image here</Typography>
-                </Box>
+
+            {imageUrl
+                ? (
+                    <Box className="uploaded-box">
+                        <img src={imageUrl} alt="uploaded file" className="uploaded-image"/>
+                    </Box>)
+                : (
+                    <>
+                        <Typography variant="body1" gutterBottom>File should be jpeg, png...</Typography>
+                        <Box className="drag-n-drop">
+                            <img src={Icon} alt="mountain" id="icon" className="drag-n-drop-image"/>
+                            <Typography variant="body2">Drag & Drop your image here</Typography>
+                        </Box>
+                    </>
+                )
+            }
             <Typography variant="body1" gutterBottom>or</Typography>
-            <Button variant="contained" color="primary">Choose a file</Button>
+            <Button component="label" variant="contained" color="primary">
+                Choose a File<input type="file" hidden onChange={handleFileSelection}/>
+            </Button>
         </Box>
     );
 }
